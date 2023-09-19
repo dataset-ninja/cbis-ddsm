5b62d1eb00227c54de95f16695b1c939c4a17bcb
erastov-alex <erastov.alex@gmail.com>
2023-09-19 10:53:46 +0300

    upd settings

--- src/settings.py
+++ src/settings.py
@@ -43,7 +43,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=22516629"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 4266633
+PREVIEW_IMAGE_ID: int = 4269384
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/cbis-ddsm"
